bc5e71255de50cb73b7130798ac85c2848b021de
TheSil <sildak@gmail.com>
2020-12-16 13:24:31 +0100

    puzzle generator cleanup

--- puzzle_generator.py
+++ puzzle_generator.py
@@ -1,74 +1,20 @@
 import random
-import itertools
+
+from core.defs import E, S, W, N
 
 # free parameters
-color_counts = []
-
-# height = width = 8
-# unique_edge_colors_count = 3
-# unique_inner_colors_count = 16
-
-# height = width = 6
-# unique_edge_colors_count = 3
-# unique_inner_colors_count = 7
-
-# height = width = 5
-# unique_edge_colors_count = 3
-# unique_inner_colors_count = 6
-
-# height = width = 16
-# unique_edge_colors_count = 5
-# unique_inner_colors_count = 17
-
-# height = width = 4
-# unique_edge_colors_count = 4
-# unique_inner_colors_count = 5
-
-# height = 8
-# width = 7
-# unique_edge_colors_count = 4
-# unique_inner_colors_count = 14
-
-# height =  5
-# width = 6
-# unique_edge_colors_count = 3
-# unique_inner_colors_count = 7
-
-# height =  18
-# width = 36
-# unique_edge_colors_count = 4
-# unique_inner_colors_count = 10
-
-# height = 7
-# width = 10
-# unique_edge_colors_count = 4
-# unique_inner_colors_count = 14
-
-height =  16
+height = 16
 width = 16
 unique_edge_colors_count = 5
 unique_inner_colors_count = 17
-color_counts = [12,12,12,12,12,24,24,24,25,25,25,24,24,25,25,25,25,25,25,25,25,25]
+# empty list will make the algorithm make any number of colors
+color_counts = [12, 12, 12, 12, 12, 24, 24, 24, 25, 25, 25, 24, 24, 25, 25, 25, 25, 25, 25, 25, 25, 25]
 
-
-
-
-
-
-
-# derived parameters
-pieces_count = height*width
-corners_count = 4
-edges_count = 2*(height-2) + 2*(width-2)
-inner_count = (height-2)*(width-2)
-edge_color_subpieces = edges_count + corners_count
-inner_color_subpieces = (height-2)*(height-1)*2
-#color_subpieces = edge_color_subpieces + inner_color_subpieces
 
 # actual pieces init - color lists in clock-wise order
 class PieceDef:
     def __init__(self, E=0, S=0, W=0, N=0):
-        self.colors = [E,S,W,N]
+        self.colors = [E, S, W, N]
 
     def get_color(self, idx):
         return self.colors[idx]
@@ -79,9 +25,6 @@ class PieceDef:
     def __repr__(self):
         return str(self.colors)
 
-corners = [PieceDef() for i in range(4)]
-edges = [PieceDef() for i in range(edges_count)]
-inner = [PieceDef() for i in range(inner_count)]
 
 # use the sequence and de-serialize it into individual pieces
 # we need to be careful so that neighbour edges match
@@ -102,135 +45,151 @@ class PieceRef:
         self.piece_def.set_color(idx, color)
 
 
-board = [width*[0] for i in range(height)]
-E = 0
-S = 1
-W = 2
-N = 3
-board[0][0] = PieceRef(corners[0], E)
-board[0][width-1] = PieceRef(corners[1], S)
-board[height-1][width-1] = PieceRef(corners[2], W)
-board[height-1][0] = PieceRef(corners[3], N)
+def generate_puzzle(height, width, unique_edge_colors_count, unique_inner_colors_count, color_counts):
+    # derived parameters
+    pieces_count = height * width
+    corners_count = 4
+    edges_count = 2 * (height - 2) + 2 * (width - 2)
+    inner_count = (height - 2) * (width - 2)
+    edge_color_subpieces = edges_count + corners_count
+    inner_color_subpieces = (height - 2) * (height - 1) * 2
+    # color_subpieces = edge_color_subpieces + inner_color_subpieces
 
-# edges
-edge_index = 0
-# upper edges
-i=0
-for j in range(1, width - 1):
-    board[i][j] = PieceRef(edges[edge_index], E)
-    edge_index += 1
-# right edges
-j = width -1
-for i in range(1, height - 1):
-    board[i][j] = PieceRef(edges[edge_index], S)
-    edge_index += 1
-# bottom edges
-i = height-1
-for j in range(1, width - 1):
-    board[i][j] = PieceRef(edges[edge_index], W)
-    edge_index += 1
-# left edges
-j = 0
-for i in range(1, height - 1):
-    board[i][j] = PieceRef(edges[edge_index], N)
-    edge_index += 1
-# inner
-inner_idx = 0
-for i in range(1, height - 1):
+    corners = [PieceDef() for _ in range(4)]
+    edges = [PieceDef() for _ in range(edges_count)]
+    inner = [PieceDef() for _ in range(inner_count)]
+
+    board = [width * [PieceRef(0, 0)] for i in range(height)]
+    E = 0
+    S = 1
+    W = 2
+    N = 3
+    board[0][0] = PieceRef(corners[0], E)
+    board[0][width - 1] = PieceRef(corners[1], S)
+    board[height - 1][width - 1] = PieceRef(corners[2], W)
+    board[height - 1][0] = PieceRef(corners[3], N)
+
+    # edges
+    edge_index = 0
+    # upper edges
+    i = 0
     for j in range(1, width - 1):
-        board[i][j] = PieceRef(inner[inner_idx], E)
-        inner_idx += 1
-
-# generate the pieces colors
-random_edge_permutation = edge_color_subpieces*[0]
-random_inner_permutation = inner_color_subpieces*[0]
-if not color_counts:
-    for i in range(edge_color_subpieces):
-        random_edge_permutation[i] = random.randint(1, unique_edge_colors_count)
-    for i in range(inner_color_subpieces):
-        random_inner_permutation[i] = random.randint(unique_edge_colors_count + 1, unique_edge_colors_count + unique_inner_colors_count)
-else:
-    random_edge_permutation = []
-    random_inner_permutation = []
-    for color_count_idx in range(unique_edge_colors_count):
-        color = color_count_idx + 1
-        random_edge_permutation += [color]*color_counts[color_count_idx]
-    for color_count_idx in range(unique_edge_colors_count, unique_inner_colors_count + unique_edge_colors_count):
-        color = color_count_idx + 1
-        random_inner_permutation += [color] * color_counts[color_count_idx]
-    random.shuffle(random_edge_permutation)
-    random.shuffle(random_inner_permutation)
-
-    #raise NotImplemented("predefined color counts not implemented")
-
-
-sequence_index = 0
-
-# edge colors
-# top edges
-i = 0
-for j in range(0, width-1):
-    color = random_edge_permutation[sequence_index]
-    left = board[i][j]
-    right = board[i][j+1]
-    left.set_color(E, color)
-    right.set_color(W, color)
-    sequence_index += 1
-
-# right edges
-j = width-1
-for i in range(0, height-1):
-    color = random_edge_permutation[sequence_index]
-    up = board[i][j]
-    down = board[i+1][j]
-    up.set_color(S, color)
-    down.set_color(N, color)
-    sequence_index += 1
-
-# bottom edges
-i = height-1
-for j in range(0, width-1):
-    color = random_edge_permutation[sequence_index]
-    left = board[i][j]
-    right = board[i][j+1]
-    left.set_color(E, color)
-    right.set_color(W, color)
-    sequence_index += 1
-
-# left edges
-j = 0
-for i in range(0, height-1):
-    color = random_edge_permutation[sequence_index]
-    up = board[i][j]
-    down = board[i+1][j]
-    up.set_color(S, color)
-    down.set_color(N, color)
-    sequence_index += 1
-
-# inner edges
-sequence_index = 0
-for i in range(1, height):
+        board[i][j] = PieceRef(edges[edge_index], E)
+        edge_index += 1
+    # right edges
+    j = width - 1
+    for i in range(1, height - 1):
+        board[i][j] = PieceRef(edges[edge_index], S)
+        edge_index += 1
+    # bottom edges
+    i = height - 1
     for j in range(1, width - 1):
-        center = board[i][j]
-        up = board[i-1][j]
+        board[i][j] = PieceRef(edges[edge_index], W)
+        edge_index += 1
+    # left edges
+    j = 0
+    for i in range(1, height - 1):
+        board[i][j] = PieceRef(edges[edge_index], N)
+        edge_index += 1
+    # inner
+    inner_idx = 0
+    for i in range(1, height - 1):
+        for j in range(1, width - 1):
+            board[i][j] = PieceRef(inner[inner_idx], E)
+            inner_idx += 1
 
-        color = random_inner_permutation[sequence_index]
-        center.set_color(N, color)
-        up.set_color(S, color)
-        sequence_index += 1
+    # generate the pieces colors
+    random_edge_permutation = edge_color_subpieces * [0]
+    random_inner_permutation = inner_color_subpieces * [0]
+    if not color_counts:
+        for i in range(edge_color_subpieces):
+            random_edge_permutation[i] = random.randint(1, unique_edge_colors_count)
+        for i in range(inner_color_subpieces):
+            random_inner_permutation[i] = random.randint(unique_edge_colors_count + 1,
+                                                         unique_edge_colors_count + unique_inner_colors_count)
+    else:
+        random_edge_permutation = []
+        random_inner_permutation = []
+        for color_count_idx in range(unique_edge_colors_count):
+            color = color_count_idx + 1
+            random_edge_permutation += [color] * color_counts[color_count_idx]
+        for color_count_idx in range(unique_edge_colors_count, unique_inner_colors_count + unique_edge_colors_count):
+            color = color_count_idx + 1
+            random_inner_permutation += [color] * color_counts[color_count_idx]
+        random.shuffle(random_edge_permutation)
+        random.shuffle(random_inner_permutation)
 
-for i in range(1, height - 1):
-    for j in range(1, width):
-        center = board[i][j]
-        left = board[i][j-1]
+        # raise NotImplemented("predefined color counts not implemented")
 
-        color = random_inner_permutation[sequence_index]
-        center.set_color(W, color)
+    sequence_index = 0
+
+    # edge colors
+    # top edges
+    i = 0
+    for j in range(0, width - 1):
+        color = random_edge_permutation[sequence_index]
+        left = board[i][j]
+        right = board[i][j + 1]
         left.set_color(E, color)
+        right.set_color(W, color)
         sequence_index += 1
+
+    # right edges
+    j = width - 1
+    for i in range(0, height - 1):
+        color = random_edge_permutation[sequence_index]
+        up = board[i][j]
+        down = board[i + 1][j]
+        up.set_color(S, color)
+        down.set_color(N, color)
+        sequence_index += 1
+
+    # bottom edges
+    i = height - 1
+    for j in range(0, width - 1):
+        color = random_edge_permutation[sequence_index]
+        left = board[i][j]
+        right = board[i][j + 1]
+        left.set_color(E, color)
+        right.set_color(W, color)
+        sequence_index += 1
+
+    # left edges
+    j = 0
+    for i in range(0, height - 1):
+        color = random_edge_permutation[sequence_index]
+        up = board[i][j]
+        down = board[i + 1][j]
+        up.set_color(S, color)
+        down.set_color(N, color)
+        sequence_index += 1
+
+    # inner edges
+    sequence_index = 0
+    for i in range(1, height):
+        for j in range(1, width - 1):
+            center = board[i][j]
+            up = board[i - 1][j]
+
+            color = random_inner_permutation[sequence_index]
+            center.set_color(N, color)
+            up.set_color(S, color)
+            sequence_index += 1
+
+    for i in range(1, height - 1):
+        for j in range(1, width):
+            center = board[i][j]
+            left = board[i][j - 1]
+
+            color = random_inner_permutation[sequence_index]
+            center.set_color(W, color)
+            left.set_color(E, color)
+            sequence_index += 1
+
+    return board, corners, edges, inner
+
 
 def validate(board, height, width):
-
     def check(color1, color2):
         if color1 != color2:
             raise Exception("Not valid color placement")
@@ -242,7 +201,7 @@ def validate(board, height, width):
             up = board[i - 1][j] if i > 0 else None
             down = board[i + 1][j] if i < N - 1 else None
             left = board[i][j - 1] if j > 0 else None
-            right = board[i][j + 1] if j < N -1 else None
+            right = board[i][j + 1] if j < N - 1 else None
 
             if up:
                 check(center.get_color(N), up.get_color(S))
@@ -253,72 +212,72 @@ def validate(board, height, width):
             if left:
                 check(center.get_color(W), left.get_color(E))
 
-validate(board, height, width)
 
-# report
-print(f"height={height}, "
-      f"width={width}, "
-      f"pieces={pieces_count}, "
-      f"corners={corners_count}, "
-      f"edges={edges_count}, "
-      f"inner={inner_count},"
-      f"edge_color_subpieces={edge_color_subpieces},"
-      f"inner_color_subpieces={inner_color_subpieces}")
+if __name__ == '__main__':
 
-print("corners:")
-for piece in corners:
-    print(piece)
+    board, corners, edges, inner = generate_puzzle(height, width, unique_edge_colors_count, unique_inner_colors_count,
+                                                   color_counts)
 
-print("edges:")
-for piece in edges:
-    print(piece)
+    validate(board, height, width)
 
-print("inner:")
-for piece in inner:
-    print(piece)
+    # report
+    print(f"height={height}, "
+          f"width={width}, "
+          f"pieces={len(corners) + len(edges) + len(inner)}, "
+          f"corners={len(corners)}, "
+          f"edges={len(edges)}, "
+          f"inner={len(inner)}")
 
-# save shuffled pieces into the config file (csv for now)
-random_corner_idxs = list(range(len(corners)))
-random.shuffle(random_corner_idxs)
-random_edge_idxs = list(range(len(edges)))
-random.shuffle(random_edge_idxs)
-random_inner_idxs = list(range(len(inner)))
-random.shuffle(random_inner_idxs)
+    print("corners:")
+    for piece in corners:
+        print(piece)
 
-with open(f"{height}_{width}_edge{unique_edge_colors_count}_inner{unique_inner_colors_count}.csv", "w") as f:
-    f.write(f"{height},{width},{unique_edge_colors_count},{unique_inner_colors_count}\n")
+    print("edges:")
+    for piece in edges:
+        print(piece)
 
-    id = 1
-    for piece_idx in random_corner_idxs:
-        piece = corners[piece_idx]
-        piece.shuffled_id = id
-        piece.shuffled_orientation = -1
-        f.write(f"{id},{piece.colors[0]},{piece.colors[1]}\n")
-        id += 1
+    print("inner:")
+    for piece in inner:
+        print(piece)
 
-    for piece_idx in random_edge_idxs:
-        piece = edges[piece_idx]
-        piece.shuffled_id = id
-        piece.shuffled_orientation = -1
-        f.write(f"{id},{piece.colors[0]},{piece.colors[1]},{piece.colors[2]}\n")
-        id += 1
+    # save shuffled pieces into the config file (csv for now)
+    random_corner_idxs = list(range(len(corners)))
+    random.shuffle(random_corner_idxs)
+    random_edge_idxs = list(range(len(edges)))
+    random.shuffle(random_edge_idxs)
+    random_inner_idxs = list(range(len(inner)))
+    random.shuffle(random_inner_idxs)
 
-    for piece_idx in random_inner_idxs:
-        piece = inner[piece_idx]
-        piece.shuffled_id = id
-        piece.shuffled_orientation = random.randint(0,3)
-        f.write(f"{id},{piece.colors[(0 + piece.shuffled_orientation) % 4]}"
-                f",{piece.colors[(1 + piece.shuffled_orientation) % 4]}"
-                f",{piece.colors[(2 + piece.shuffled_orientation) % 4]}"
-                f",{piece.colors[(3 + piece.shuffled_orientation) % 4]}\n")
-        id += 1
+    with open(f"{height}_{width}_edge{unique_edge_colors_count}_inner{unique_inner_colors_count}.csv", "w") as f:
+        f.write(f"{height},{width},{unique_edge_colors_count},{unique_inner_colors_count}\n")
 
-with open(f"{height}_{width}_edge{unique_edge_colors_count}_inner{unique_inner_colors_count}_solution.csv", "w") as f:
-    for i in range(0, height):
-        for j in range(0, width):
-            f.write(f"{i},{j},{board[i][j].piece_def.shuffled_id},{board[i][j].piece_def.shuffled_orientation}\n")
+        id = 1
+        for piece_idx in random_corner_idxs:
+            piece = corners[piece_idx]
+            piece.shuffled_id = id
+            piece.shuffled_orientation = -1
+            f.write(f"{id},{piece.colors[0]},{piece.colors[1]}\n")
+            id += 1
 
+        for piece_idx in random_edge_idxs:
+            piece = edges[piece_idx]
+            piece.shuffled_id = id
+            piece.shuffled_orientation = -1
+            f.write(f"{id},{piece.colors[0]},{piece.colors[1]},{piece.colors[2]}\n")
+            id += 1
 
+        for piece_idx in random_inner_idxs:
+            piece = inner[piece_idx]
+            piece.shuffled_id = id
+            piece.shuffled_orientation = random.randint(0, 3)
+            f.write(f"{id},{piece.colors[(0 + piece.shuffled_orientation) % 4]}"
+                    f",{piece.colors[(1 + piece.shuffled_orientation) % 4]}"
+                    f",{piece.colors[(2 + piece.shuffled_orientation) % 4]}"
+                    f",{piece.colors[(3 + piece.shuffled_orientation) % 4]}\n")
+            id += 1
 
-
-
+    with open(f"{height}_{width}_edge{unique_edge_colors_count}_inner{unique_inner_colors_count}_solution.csv",
+              "w") as f:
+        for i in range(0, height):
+            for j in range(0, width):
+                f.write(f"{i},{j},{board[i][j].piece_def.shuffled_id},{board[i][j].piece_def.shuffled_orientation}\n")
