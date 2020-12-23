45e7afa1746792cdda9c09eb12477d65d71730cc
TheSil <sildak@gmail.com>
2020-12-24 00:26:11 +0100

    swapper auto reset when score does not change (trying many different variants in short time)

--- core_cpp/main.cpp
+++ core_cpp/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
     edge::PuzzleDef def = edge::PuzzleDef::Load(def_file, hints_file);
     edge::Board board(&def);
 
-#if 1 // BACKTRACKER
+#if 0 // BACKTRACKER
 
     std::set<std::pair<int, int>>* pMap = nullptr;
     //tested fields map
@@ -132,92 +132,109 @@ int main(int argc, char* argv[])
 
 #endif
 
-#if 0 // swapper
+#if 1 // swapper
 
-    std::string load_file = "";
-    if (argc > 3) {
-        load_file = argv[3];
-        board.Load(load_file);
-        // some save files have missing pieces,
-        // but swapper expects filled board
-        for (auto& piece : def.GetCorners())
-        {
-            if (!board.GetLocations()[piece.id]) {
-                for (auto& coord : board.GetCornersCoords()) {
-                    auto loc = board.GetLocation(coord.first, coord.second);
-                    if (!loc->ref) {
-                        board.PutPiece(piece.id, loc->x, loc->y, 0);
+
+    while (true)
+    {
+        std::string load_file = "";
+        if (argc > 3) {
+            load_file = argv[3];
+            board.Load(load_file);
+            // some save files have missing pieces,
+            // but swapper expects filled board
+            for (auto& piece : def.GetCorners())
+            {
+                if (!board.GetLocations()[piece.id]) {
+                    for (auto& coord : board.GetCornersCoords()) {
+                        auto loc = board.GetLocation(coord.first, coord.second);
+                        if (!loc->ref) {
+                            board.PutPiece(piece.id, loc->x, loc->y, 0);
+                        }
                     }
                 }
             }
-        }
-        for (auto& piece : def.GetEdges())
-        {
-            if (!board.GetLocations()[piece.id]) {
-                for (auto& coord : board.GetEdgesCoords()) {
-                    auto loc = board.GetLocation(coord.first, coord.second);
-                    if (!loc->ref) {
-                        board.PutPiece(piece.id, loc->x, loc->y, 0);
+            for (auto& piece : def.GetEdges())
+            {
+                if (!board.GetLocations()[piece.id]) {
+                    for (auto& coord : board.GetEdgesCoords()) {
+                        auto loc = board.GetLocation(coord.first, coord.second);
+                        if (!loc->ref) {
+                            board.PutPiece(piece.id, loc->x, loc->y, 0);
+                        }
                     }
                 }
             }
-        }
-        for (auto& piece : def.GetInner())
-        {
-            if (!board.GetLocations()[piece.id]) {
-                for (auto& coord : board.GetInnersCoords()) {
-                    auto loc = board.GetLocation(coord.first, coord.second);
-                    if (!loc->ref) {
-                        board.PutPiece(piece.id, loc->x, loc->y, 0);
+            for (auto& piece : def.GetInner())
+            {
+                if (!board.GetLocations()[piece.id]) {
+                    for (auto& coord : board.GetInnersCoords()) {
+                        auto loc = board.GetLocation(coord.first, coord.second);
+                        if (!loc->ref) {
+                            board.PutPiece(piece.id, loc->x, loc->y, 0);
+                        }
                     }
                 }
             }
+            board.AdjustDirBorder();
         }
-        board.AdjustDirBorder();
-    }
-    else {
-        board.Randomize();
-        board.AdjustDirBorder();
-        board.AdjustDirInner();
-    }
+        else {
+            board.Randomize();
+            board.AdjustDirBorder();
+            board.AdjustDirInner();
+        }
 
-    int i = 0;
-    int start = (int)time(0);
-    int score = board.GetScore();
-    int max_score = score;
-    printf("score: %i\n", score);
-    std::string last_save = "";
+        int i = 0;
+        int start = (int)time(0);
+        const int restart_delay = 10;
+        int restart_time = (int)time(0) + restart_delay;
+        int minimal_save_score = 456;
+        int score = board.GetScore();
+        int max_score = score;
+        printf("score: %i\n", score);
+        std::string last_save = "";
 
-    edge::Swapper swapper(board);
-    while (true) {
-        swapper.DoSwap();
-        score = board.GetScore();
-        if (board.GetScore() > max_score) {
-            max_score = score;
-            if (score > 455) {
-                try {
-                    remove(last_save.c_str());
+
+        edge::Swapper swapper(board);
+        while (true) {
+            swapper.DoSwap();
+            score = board.GetScore();
+            if (board.GetScore() > max_score) {
+                max_score = score;
+                if (score > minimal_save_score) {
+                    try {
+                        remove(last_save.c_str());
+                    }
+                    catch (...) {
+
+                    }
+                    std::stringstream ss;
+                    ss << prefix << "_swapper_save_" << score << ".csv";
+                    last_save = ss.str();
+                    board.Save(last_save);
                 }
-                catch (...) {
 
-                }
-                std::stringstream ss;
-                ss << prefix << "_swapper_save_" << score << ".csv";
-                last_save = ss.str();
-                board.Save(last_save);
+                // score improved, delay restart timer
+                restart_time = (int)time(0) + restart_delay;
             }
-        }
 
-        i += 1;
-        //if (i % 5 == 0) {
+            if (time(0) > restart_time /*&& max_score < minimal_save_score*/) {
+                printf("score did not change too long, restarting\n");
+                break;
+            }
+
+            i += 1;
+            //if (i % 5 == 0) {
             Sleep(1);
-        //}
-        //int now = (int)time(0);
-        //if (now - start >= 1) {
-        //    printf("%i iterations/s\n", i);
-        //    i = 0;
-        //    start = now;
-        //}
+            //}
+            //int now = (int)time(0);
+            //if (now - start >= 1) {
+            //    printf("%i iterations/s\n", i);
+            //    i = 0;
+            //    start = now;
+            //}
+        }
+
     }
 
 #endif
