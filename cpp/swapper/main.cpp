68f7bcb9733edf5525a275e92d54b744ba759901
TheSil <sildak@gmail.com>
2020-12-30 18:25:53 +0100

    cpp project/folders reorg

--- cpp/swapper/main.cpp
+++ cpp/swapper/main.cpp
@@ -3,67 +3,8 @@
 #include "PuzzleDef.h"
 #include "Board.h"
 #include "Swapper.h"
-#include "Backtracker.h"
 #include <time.h>
 #include <Windows.h>
-
-class NewBest : public edge::backtracker::CallbackOnSolve {
-public:
-    NewBest(const std::string& prefix) : prefix(prefix), counter(0), max_score(0)
-    {
-    }
-
-    void Call(edge::Board& board)
-    {
-        int score = board.GetScore();
-        if (score > max_score) {
-            max_score = score;
-        }
-        printf("New best backstack position reached, score: %i\n", score);
-        if (score > 330/*420*/) {
-            std::stringstream ss;
-            try {
-                remove(last_save.c_str());
-            }
-            catch (...) {
-
-            }
-            ss << prefix << "_backtracker_save_" << score << ".csv";
-            last_save = ss.str();
-            board.Save(last_save);
-        }
-    }
-
-    int max_score;
-
-private:
-    std::string prefix;
-    int counter;
-    std::string last_save;    
-};
-
-class Solved : public edge::backtracker::CallbackOnSolve {
-public:
-    Solved(const std::string& prefix) : prefix(prefix), counter(0)
-    {
-    }
-    
-    void Call(edge::Board& board)
-    {
-        printf("SOLVED!\n");
-        if (counter < 20)
-        {// safety mechanism, do not save more than certain number of solutions...
-            std::stringstream ss;
-            ss << prefix << "_save_" << "solved_" << ++counter << ".csv";
-            board.Save(ss.str());
-        }
-
-    }
-
-private:
-    std::string prefix;
-    int counter;
-};
 
 int main(int argc, char* argv[])
 {
@@ -104,77 +45,7 @@ int main(int argc, char* argv[])
     edge::PuzzleDef def = edge::PuzzleDef::Load(def_file, hints_file);
     edge::Board board(&def);
 
-#if 1 // BACKTRACKER
-
-    std::set<std::pair<int, int>>* pMap = nullptr;
-    //tested fields map
-    //std::set<std::pair<int, int>> map;
-    //for (int x = 0; x < def.GetHeight(); ++x) {
-    //    for (int y = 0; y < def.GetWidth(); ++y) {
-    //        if (x <= 2 || y <= 2 || x >= def.GetHeight() - 3 || y >= def.GetWidth() - 3)
-    //            map.insert(std::pair<int,int>(x,y));
-    //    }
-    //}
-    //pMap = &map;
-
-    Solved solved_callback(prefix);
-    NewBest newbest_callback(prefix);
-    edge::backtracker::Backtracker backtracker(board, pMap, true, rotations_file);
-    backtracker.RegisterOnSolve(&solved_callback);
-    backtracker.RegisterOnNewBest(&newbest_callback);
-
-    int i = 0;
-    int start = (int)time(0);
-    int start_absolute = start;
-    int score = 0;
-    int prev_counter = 0;
-    int max_score = 0;
-    printf("score: %i\n", score);
-    while (backtracker.Step()) {
-
-        i += 1;
-        //if (i % 5 == 0) {
-        //    Sleep(1);
-        //}
-        if (backtracker.GetCounter() != prev_counter) {
-            prev_counter = backtracker.GetCounter();
-            printf("counter: %i \n", prev_counter);
-        }
-
-        int now = (int)time(0);
-        if (now - start >= 1) {
-            std::string explAbsLast, explAbs, explMax, explRatio;
-            backtracker.GetStats().GetExploredAbsLast().PrintExp(explAbsLast);
-            backtracker.GetStats().GetExploredAbs().PrintExp(explAbs);
-            backtracker.GetStats().GetExploredRatio().PrintExp(explRatio);
-            backtracker.GetStats().GetExploredMax().PrintExp(explMax);
-
-            score = board.GetScore();
-             printf("max_score: %i, curr_score: %i, iters: %i, "
-                "explAbsLast: %s, explAbs: %s, explRatio: %s, explMax: %s\n", 
-                newbest_callback.max_score, score, i, explAbsLast.c_str(), explAbs.c_str(), explRatio.c_str(), explMax.c_str());
-
-            Sleep(10);
-            i = 0;
-            start = now;
-        }
-    }
-
-    printf("finished in %i sec\n", (int)time(0) - start_absolute);
-    std::string explAbsLast, explAbs, explMax, explRatio;
-    backtracker.GetStats().GetExploredAbsLast().PrintExp(explAbsLast);
-    backtracker.GetStats().GetExploredAbs().PrintExp(explAbs);
-    backtracker.GetStats().GetExploredRatio().PrintExp(explRatio);
-    backtracker.GetStats().GetExploredMax().PrintExp(explMax);
-
-    printf("max_score: %i, curr_score: %i, iters: %i, "
-        "explAbsLast: %s, explAbs: %s, explRatio: %s, explMax: %s\n",
-        max_score, score, i, explAbsLast.c_str(), explAbs.c_str(), explRatio.c_str(), explMax.c_str());
-
-#endif
-
-#if 0 // swapper
-
+#if 1 // swapper
 
     while (true)
     {
